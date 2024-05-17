676e6208e808f60711a57a3ecf80d662139d0cb6
LucasEspLau <lukaespinoza12@gmail.com>
2024-05-16 19:03:57 -0500

    test deploy

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from 'react';
+
+function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://cris.unfv.edu.pe/ws/api/research-outputs?size=10", {
+          method: "GET",
+          headers: {
+            "api-key": "763e40e0-407e-41c3-b1d4-7cfe70a85b03"
+          }
+        });
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      {data ? (
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+}
+
+export default App;
